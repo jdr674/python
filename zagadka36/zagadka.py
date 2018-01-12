8d279945b28302f94d789535123170e2ab77236b
Mariusz Wojciechowski <JDR674@motorolasolutions.com>
2018-01-12 20:37:21 +0100

    Final

--- zagadka36/zagadka.py
+++ zagadka36/zagadka.py
@@ -1,9 +1,19 @@
 # ROzwiazanie zagadki "36"
 
-print """
+print ("""
 
     ZAGADKA '36'
 
-    """
+    """);
 
 
+p=1; d=1;t=1;
+iloczyn=36;
+
+for p in range (1, 37):
+    for d in range (1, 37):
+        for t in range (1, 37):
+           if p*d*t==iloczyn:
+               print (" Udalo sie znalesc:", p, " ", d ," ", t, " Suma:", p+d+t);
+
+print ("Koniec");
